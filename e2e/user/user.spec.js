3d4555c59ad5760d4ae911be533a38c67dbbb865
AdrianiBruss <abrussolo@gmail.com>
2015-05-05 10:52:52 +0200

    added e2e test to login page

---
+++ e2e/user/user.spec.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Main View', function() {
+  var page;
+
+  beforeEach(function() {
+    browser.get('/login');
+    page = require('./user.po');
+  });
+
+  it('should login and redirect to home', function() {
+    page.password.sendKeys('admin');
+    page.email.sendKeys('admin@admin.com');
+    //page.name.sendKeys('admin');
+    page.goButton.click().then(function (){
+      browser.waitForAngular();
+      expect(browser.getLocationAbsUrl()).toBe('/');
+    })
+  });
+});
